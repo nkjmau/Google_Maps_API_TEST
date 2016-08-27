49dd48ce99c7bf638073384bcf6d6b3f712e9c50
nkjmau <nkjmau@yahoo.co.jp>
2016-08-28 01:41:01 +0900

    googleMapOptの初期化

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,4 +1,14 @@
-var googleMapOpts;
+var googleMapOpts = {
+  zoom: 18,
+  center: , //現在地geolocationAPIより
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  draggable: false,  // mapのドラッグ無効化オプション
+  disableDoubleClickZoom: true,  //ダブルクリック時のズーム無効化
+  mapTypeControl: false, //航空写真or地図の選択ボックス非表示
+  streetViewControl: false,  //streetViewの無効化
+  zoomControl: false,
+  scrollwheel: false  //スクロールによるズーム無効化
+};
 
 window.onload = function(){
   if(navigator.geolocation){
@@ -6,19 +16,7 @@ window.onload = function(){
       // 位置情報取得成功時
       function(position){
         // GoogleMapの表示
-        var latlng = new google.maps.LatLng( position.coords.latitude, position.coords.longitude);
-        //map表示オプション
-        googleMapOpts = {
-          zoom: 18,
-          center: latlng, //現在地geolocationAPIより
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          draggable: false,  // mapのドラッグ無効化オプション
-          disableDoubleClickZoom: true,  //ダブルクリック時のズーム無効化
-          mapTypeControl: false, //航空写真or地図の選択ボックス非表示
-          streetViewControl: false,  //streetViewの無効化
-          zoomControl: false,
-          scrollwheel: false  //スクロールによるズーム無効化
-        };
+        googleMapOpts.center = new google.maps.LatLng( position.coords.latitude, position.coords.longitude);
         var map = new google.maps.Map(document.getElementById("map_canvas"), googleMapOpts);
       },
       // 位置情報取得失敗時
@@ -47,9 +45,7 @@ function showMap() {
   navigator.geolocation.getCurrentPosition(
     function (position){
       // GoogleMapの表示
-      var latlng = new google.maps.LatLng( position.coords.latitude, position.coords.longitude);
-      //map表示オプション
-      googleMapOpts.center = latlng;
+      googleMapOpts.center = new google.maps.LatLng( position.coords.latitude, position.coords.longitude);
       var map = new google.maps.Map(document.getElementById("map_canvas"), googleMapOpts);
     }
   );
