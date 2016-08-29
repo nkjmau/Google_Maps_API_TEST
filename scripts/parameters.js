7234b4b3f79f096bed604333cefe22cee4adea9d
nkjmau <nkjmau@yahoo.co.jp>
2016-08-29 12:02:30 +0900

    delete display of parameters

--- scripts/parameters.js
+++ scripts/parameters.js
@@ -6,10 +6,6 @@ var dist = 0;
 window.onload = function () {
   var out = document.getElementById("lvl");
   out.innerHTML = level;
-  out = document.getElementById("resistance");
-  out.innerHTML = resistance;
-  out = document.getElementById("HP");
-  out.innerHTML = hitPoint;
   out = document.getElementById("dist");
   out.innerHTML = parseFloat(dist).toFixed(1) + "km";
 }
@@ -19,14 +15,10 @@ function levelUp(){
   var out = document.getElementById("lvl");
   out.innerHTML = level;
   resistance += 2;
-  out = document.getElementById("resistance");
-  out.innerHTML = resistance;
 }
 
 function HPchange(num){
   if(num == 1)  hitPoint++;
   else if(num == 2)  hitPoint -= resistance;
   if(hitPoint > 1000)  hitPoint = 1000;
-  var out = document.getElementById("HP");
-  out.innerHTML = hitPoint;
 }
