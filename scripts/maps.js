dadfd5e08f86dddfa36d957edada2b4a4382d427
nkjmau <nkjmau@yahoo.co.jp>
2016-08-31 13:47:10 +0900

    実験

--- scripts/maps.js
+++ scripts/maps.js
@@ -10,9 +10,9 @@ var googleMapOpts = {
   scrollwheel: false  //スクロールによるズーム無効化
 };
 
-var map;
-var latitude, longitude;
 window.addEventListener('load', function(){
+  var map;
+  var latitude, longitude;
   if (navigator.geolocation){
     // 位置情報を取得
     // TODO: 取得したら(一定時間ごとに？)送信
@@ -26,33 +26,33 @@ window.addEventListener('load', function(){
     alert('cannot get geolocation');
     return;
   }
+  function showMap(){
+    var googleMapOpts = {
+      zoom: 18,
+      center: new google.maps.LatLng( latitude, longitude), //現在地geolocationAPIより
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      draggable: false,  // mapのドラッグ無効化オプション
+      disableDoubleClickZoom: true,  //ダブルクリック時のズーム無効化
+      mapTypeControl: false, //航空写真or地図の選択ボックス非表示
+      streetViewControl: false,  //streetViewの無効化
+      zoomControl: false,
+      scrollwheel: false  //スクロールによるズーム無効化
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), googleMapOpts);
+  }
+  function updateMap(){
+    map.setCenter(new google.maps.LatLng( latitude, longitude));
+  }
+  
+  window.addEventListener('click', function() {
+    showMap();
+    setInterval(function() {
+      console.log(latitude, longitude);
+      updateMap();
+    }, 3000);
+    window.removeEventListener('click', arguments.callee);
+  });
 });
-
-window.addEventListener('click', function() {
-  showMap();
-  setInterval(function() {
-    console.log(latitude, longitude);
-    updateMap();
-  }, 3000);
-  window.removeEventListener('click', arguments.callee);
-});
-function showMap(){
-  var googleMapOpts = {
-    zoom: 18,
-    center: new google.maps.LatLng( latitude, longitude), //現在地geolocationAPIより
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    draggable: false,  // mapのドラッグ無効化オプション
-    disableDoubleClickZoom: true,  //ダブルクリック時のズーム無効化
-    mapTypeControl: false, //航空写真or地図の選択ボックス非表示
-    streetViewControl: false,  //streetViewの無効化
-    zoomControl: false,
-    scrollwheel: false  //スクロールによるズーム無効化
-  };
-  map = new google.maps.Map(document.getElementById("map_canvas"), googleMapOpts);
-}
-function updateMap(){
-  map.setCenter(new google.maps.LatLng( latitude, longitude));
-}
 
 // ポップアップを消す
 window.addEventListener('click' , function() {
